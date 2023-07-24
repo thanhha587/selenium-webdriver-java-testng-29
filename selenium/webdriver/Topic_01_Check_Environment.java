ced943b2130d32856a1de4f681b0aaf6be84744e
thanhha <thanhha587@gmail.com>
2023-07-24 08:36:36 +0700

    tập commit khi đang conflict 4

--- selenium/webdriver/Topic_01_Check_Environment.java
+++ selenium/webdriver/Topic_01_Check_Environment.java
@@ -36,7 +36,10 @@ public class Topic_01_Check_Environment {
     public void TC_01_Url() {
         Assert.assertEquals(driver.getCurrentUrl(), "https://www.facebook.com/");
     }
-    
+    @Test
+    public void TC_05_Url() {
+        Assert.assertEquals(driver.getCurrentUrl(), "https://www.facebook.com/");
+    }
     @Test
     public void TC_02_Logo() {
         Assert.assertTrue(driver.findElement(By.cssSelector("img.fb_logo")).isDisplayed());
